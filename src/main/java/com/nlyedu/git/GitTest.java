d0834e1652487e714dd620d48e4c8716c60fcf79
Seven <Seven@nly.com>
2023-06-30 15:40:12 +0800

    first commit

---
+++ src/main/java/com/nlyedu/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.nlyedu.git;
+
+/**
+ * @author Seven
+ * @version 1.0
+ */
+public class GitTest {
+
+
+	public static void main(String[] args) {
+		System.out.println("hello,git!");
+	}
+
+}
